1f2b6be3783a055c47e8808f94911d79f2a79702
isaacquinapallo <isaac.quinapallo@epn.edu.ec>
2024-05-31 12:41:16 -0500

    añadir variables de Triangulos

--- src/FigurasGeometricas/Triangulos.java
+++ src/FigurasGeometricas/Triangulos.java
@@ -1,4 +1,17 @@
 package FigurasGeometricas;
 
-public class Triangulos {
+public class Triangulos extends regulares {
+    Double base;
+    Double altura;
+    Double area;
+    Double perimetro;
+
+    public Triangulos() {}
+    public Triangulos(){
+
+    }
+
+    public class Imprimir_Triangulos_Regulares{
+
+    }
 }
