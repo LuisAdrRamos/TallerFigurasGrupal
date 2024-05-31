dd7f803fa9951a057ce6fd3bb35936fac7fa6bbe
Luis Adrian Ramos <luchoramos042004@gmail.com>
2024-05-31 11:58:17 -0500

    ClasePadre

---
+++ src/FigurasGeometricas/FIGURASGEOMETRICAS.java
@@ -0,0 +1,30 @@
+package FigurasGeometricas;
+
+public class FIGURASGEOMETRICAS {
+    int lados;
+    String nombre;
+
+    public FIGURASGEOMETRICAS() {
+    }
+
+    public FIGURASGEOMETRICAS(int lados, String nombre) {
+        this.lados = lados;
+        this.nombre = nombre;
+    }
+
+    public int getLados() {
+        return lados;
+    }
+
+    public void setLados(int lados) {
+        this.lados = lados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
