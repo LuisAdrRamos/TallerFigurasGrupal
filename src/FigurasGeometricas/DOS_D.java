c7a136c0ac6bf7df06549bedf1ec7fc35460527b
CarlosPerez19 <151254507+CarlosPerez19@users.noreply.github.com>
2024-05-31 12:06:37 -0500

    Clase 2d

---
+++ src/FigurasGeometricas/DOS_D.java
@@ -0,0 +1,40 @@
+package FigurasGeometricas;
+
+public class DOS_D extends FIGURASGEOMETRICAS{
+
+    double area;
+    double perimetro;
+
+    public DOS_D() {
+    }
+
+    public DOS_D(double area, double perimetro) {
+        this.area = area;
+        this.perimetro = perimetro;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public double calcular_area () {
+
+    }
+
+    public double calcular_perimetro () {
+
+    }
+}
+
