6137dec59c88b5ec603ba5945a05561a1afc9abc
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-06-03 18:04:20 -0500

    Figuras regulares nLados

--- src/FigurasGeometricas/nLados.java
+++ src/FigurasGeometricas/nLados.java
@@ -2,8 +2,6 @@ package FigurasGeometricas;
 
 public class nLados extends Otras_Figuras{
     int nLados;
-    public nLados() {
-    }
 
     public nLados(int nLados) {
         this.nLados = nLados;
@@ -16,5 +14,25 @@ public class nLados extends Otras_Figuras{
     public void setnLados(int nLados) {
         this.nLados = nLados;
     }
+
+    //Triangulo 3 lados
+
+    // Cuadrado 4 lados
+
+    // Pentagono 5 lados
+
+    // Hexagono 6 lados
+
+    // Heptagono 7 lados
+
+    // Octagono 8 lados
+
+    // Nonagono 9 lados
+
+    // Decagono 10 lados
+
+    // Undecagono 11 lados
+
+    // Dodecagono 12 lados
 
 }
